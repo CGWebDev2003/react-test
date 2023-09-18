765094f82421d857feb5cfd26a95ecd24102eaf6
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2023-09-18 10:45:08 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Logo() {
+  return (
+    <>
+      <img src="../WBS_Logo_white_web.png" className="logo" alt="WBS Logo" />
+    </>
+  );
+}
+
+export default function Header() {
+  return (
+    <>
+      <div className="myheader">
+        <Logo />
+      </div>
+    </>
+  );
+}
---
+++ src/components/button.jsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <>
+      <button className="myButton">My Button</button>
+    </>
+  );
+}
